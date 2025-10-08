4d1a6fc009dfbb46f0a8b20c6a130559ac3fec65
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-10-08 11:02:29 -0400

    tyui

--- llm/pages/index.vue
+++ llm/pages/index.vue
@@ -6,10 +6,10 @@
         <div class="mr-6 hidden h-36 w-1 bg-[var(--primary)] md:block 2xl:h-44"></div>
         <h1 class="flex w-96 flex-col text-center text-3xl font-semibold leading-relaxed sm:text-4xl md:text-left md:text-5xl 2xl:text-6xl">
           <span class="text-2xl 2xl:text-4xl">Welcome to the</span>
-          SITHS Regents Prep App
+          SITHS Levina Language Model
         </h1>
       </div>
-      <p class="mb-2 mt-4 text-xl sm:mb-8 sm:text-2xl">Don't fail your Regents with this one.</p>
+      <p class="mb-2 mt-4 text-xl sm:mb-8 sm:text-2xl">Do NOT go failing Levina now.</p>
       <!-- buttons -->
       <div class="mt-4 flex space-x-4 sm:mt-0">
         <NuxtLink to="/login" class="flex items-center rounded-xl bg-[var(--primary)] px-6 py-2 text-2xl text-[var(--text-color)] hover:brightness-[0.85] lg:px-16 hover:dark:brightness-125">
