6f41ca928228f618a1b07516be37abeb1f0fecb7
rylan-geykhman <johnsmith102508@gmail.com>
2025-10-22 11:03:31 -0400

    commit

--- pages/teacher/classView.vue
+++ pages/teacher/classView.vue
@@ -9,9 +9,9 @@
 
 
        <div class="flex items-center justify-center gap-4">
-           <ActionButton type="link" :to="`/teacher/course/{classCodePlaceholder}/roster`" img="" text="View Students"/>
-           <ActionButton type="link" :to="`/teacher/create-assignment?course={classCodePlaceholder}`" img="" text="View Students"/>
-           <ActionButton type="button" img="" text="Delete Course" class="hover:bg-red-400" @on-click="" />
+           <TeacherDashboardActionButton type="link" :to="`/teacher/course/{classCodePlaceholder}/roster`" img="" text="View Students"/>
+           <TeacherDashboardActionButton type="link" :to="`/teacher/create-assignment?course={classCodePlaceholder}`" img="" text="View Students"/>
+           <TeacherDashboardActionButton type="button" img="" text="Delete Course" class="hover:bg-red-400" @on-click="" />
        </div>
      </div>
 
@@ -48,56 +48,15 @@
          </div>
        </div>-->
        <!--next part is a sample assignment card, will make into a component after i make page look how it should-->
-         <div class="relative w-full">
-    <div class="absolute right-2 top-2 z-10">
-      <img
-        tabindex="0"
-        role="button"
-        src="/ui/ellipsis-vertical.svg"
-        alt="More options"
-        class="h-7 w-7 cursor-pointer select-none rounded-full p-1 hover:bg-neutral-200 dark:hover:bg-neutral-700"
-        @click.stop="isOpen = !isOpen"
-      />
-      <Transition name="slide-up">
-        <ul v-show="isOpen" tabindex="0" class="absolute right-0 z-20 mt-2 w-52 rounded-lg border border-[var(--border-color)] bg-body p-2 shadow-lg" @click.stop>
-          <li>
-            <button class="h-10 w-full rounded-lg pl-4 text-left transition-all hover:bg-red-400/70" type="button" @click="">Delete</button>
-          </li>
-        </ul>
-      </Transition>
-    </div>
-    <NuxtLink
-      class="relative flex h-fit w-full flex-col items-center justify-center gap-2 rounded-2xl border border-neutral-300 py-4 hover:border-neutral-600 hover:shadow-lg hover:transition sm:flex-row sm:p-6 dark:border-neutral-600 dark:hover:border-neutral-300"
-      :to="`/teacher/course/{coursePlaceholder}/{assignmentPlaceholder}`"
-    >
-      <div class="flex w-5/6 flex-col items-start justify-start">
-        <h3 class="w-full overflow-hidden overflow-ellipsis text-nowrap text-2xl font-semibold">
-          Audio Thing
-        </h3>
-        <ClientOnly>
-          <!--<p :title="assignment.dueDate.toLocaleString()">Due {{ formatDate(assignment.dueDate, currentDate) }}</p>-->
-          <p>Due 10/25/2030</p>
-        </ClientOnly>
-      </div>
-
-      <div class="flex w-5/6 flex-col items-center justify-center gap-1">
-        <p class="text-xl font-medium">
-          Submissions: {{ 1 }}/{{ 2 }}
-          
-          <span class="text-sm">students</span>
-        </p>
-        <div class="relative h-2 w-full overflow-hidden rounded-full border border-neutral-300 bg-neutral-100/25">
-          <!--<div class="absolute left-0 top-0 h-full" :style="{ width: `${(assignment.numSubmitted / course.numStudents) * 100}%`, backgroundColor: subjectColors[course.subject] }"></div>
-        --></div>
-      </div>
-    </NuxtLink>
-  </div>
+       <TeacherDashboardAssignmentCard/>
    </div>
  </div>
 </template>
 
 
 <script setup lang="ts">
+import { TeacherDashboardActionButton } from '#components';
+
 
 
 
