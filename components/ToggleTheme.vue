9a6eed069076fad9558ad9b9c3c4feca954de9f3
gliberov <124058342+gliberov@users.noreply.github.com>
2025-10-26 19:47:29 -0400

    aa

---
+++ components/ToggleTheme.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="du-tooltip du-tooltip-bottom flex size-9 cursor-pointer items-center justify-center rounded-full hover:bg-neutral-200 dark:hover:bg-neutral-700"
+    :data-tip="isDarkMode ? 'Dark' : 'Light'"
+  >
+    <label class="du-swap du-swap-rotate">
+      <input type="checkbox" class="theme-controller" value="synthwave" @click="isDarkMode = !isDarkMode" />
+      <img class="du-swap-off size-9 fill-current p-1 dark:invert" src="/ui/sun.svg" alt="Switch to dark mode" draggable="false" />
+      <img class="du-swap-on size-9 fill-current p-1 dark:invert" src="/ui/moon.svg" alt="Switch to light mode" draggable="false" />
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+const userStore = useUserStore();
+const { isDarkMode } = storeToRefs(userStore);
+</script>
+
+<style scoped></style>
