5cd2e1ce4baad594767f582a91f3fec9623aa4fa
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-10-31 11:01:44 -0400

    need more fixing

--- middleware/01.init.global.ts
+++ middleware/01.init.global.ts
@@ -4,15 +4,19 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const nuxtApp = useNuxtApp();
   const userStore = useUserStore();
   
+  console.log("Running global middleware: 01.init.global");
+  console.log(userStore.isAuth, to.meta.requiresAuth, to.meta.redirectIfAuth);
   if (userStore.isAuth && to.meta.redirectIfAuth) return await navigateTo(`/${userStore.userType}/dashboard`, { redirectCode: 301 });
   
   // only run on initial page load
   // https://nuxt.com/docs/guide/directory-structure/middleware#when-middleware-runs'
   
-  if (!nuxtApp.isClient || !nuxtApp.isHydrating || !nuxtApp.payload.serverRendered) return;
+  // if (!nuxtApp.isClient || !nuxtApp.isHydrating || !nuxtApp.payload.serverRendered) return;
 
   const { error } = await tryCatch(userStore.init());
   if (error) console.error(error);
+
+  console.log(userStore.isAuth, to.meta.requiresAuth, to.meta.redirectIfAuth);
 
   if (!userStore.isAuth && to.meta.requiresAuth) return await navigateTo("/login", { redirectCode: 301 });
   else if (userStore.isAuth && to.meta.redirectIfAuth) return await navigateTo(`/${userStore.userType}/dashboard`, { redirectCode: 301 });
