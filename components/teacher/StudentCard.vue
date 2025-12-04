b8e3758115446cc44bb14c1b33ee8864a67f992e
christina chen <158211764+christinac09@users.noreply.github.com>
2025-12-04 11:02:15 -0500

    im so lost whys there so much stuff in this repo

---
+++ components/teacher/StudentCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-row">
+{{ studentName }}
+<button @click="handleDeleteStudent"><img src="/close.svg" aria-hidden="true" draggable="false" class="dark:invert"></button>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    studentName: string 
+}>()
+
+function handleDeleteStudent(){}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/teacher/roster.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <StudentCard v-for="student in students" :studentName="student.name"/>
+    </div>
+</template>
+
+<script setup lang="ts">
+const students = [{name: "John Doe"}]
+</script>
+
+<style scoped>
+
+</style>
