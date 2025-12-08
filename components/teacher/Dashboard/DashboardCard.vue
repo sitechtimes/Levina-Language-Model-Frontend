7131390b518f81c47603346ef23b69caa3cb2c28
christina chen <158211764+christinac09@users.noreply.github.com>
2025-12-08 11:02:49 -0500

    use color in template

--- components/teacher/Dashboard/DashboardCard.vue
+++ components/teacher/Dashboard/DashboardCard.vue
@@ -1,10 +1,10 @@
 <template>
   <NuxtLink
     :to="`/teacher/course/${course.id}`"
-    class="flex w-115 flex-col items-center justify-center overflow-hidden rounded-xl border border-neutral-300 bg-body hover:border-neutral-600/50 hover:shadow-lg dark:border-neutral-600 dark:hover:border-neutral-300/50"
+    class="flex w-115 flex-col items-center justify-center overflow-hidden rounded-xl bg-body border border-neutral-300 hover:border-neutral-600/50 hover:shadow-lg dark:border-neutral-600 dark:hover:border-neutral-300/50"
   >
     <!-- course information -->
-    <div class="flex h-24 w-full flex-col items-center justify-end p-2" :style="{ backgroundColor: classColors[course.classType as keyof typeof classColors] }">
+    <div class="flex h-24 w-full flex-col items-center justify-end p-2 bg-regular"  :style="{ backgroundColor: classColors[course.classType as keyof typeof classColors] }">
       <h2 :title="course.name" class="w-72 overflow-hidden overflow-ellipsis text-nowrap text-center text-2xl font-semibold">{{ course.name }}</h2>
       <p class="text-sm">Period {{ course.period }}</p>
       <p>{{ course.teacher }}</p>
@@ -30,8 +30,27 @@
 </template>
 
 <script setup lang="ts">
-defineProps<{ course: TeacherCourse }>();
+const props = defineProps<{ course: TeacherCourse }>();
 const userStore = useUserStore();
+console.log(props.course.class_type) //why does't classType work
+
+const classTypes = {
+  Regular: ["Freshman Russian", "Sophomore Russian", "Junior Russian"],
+  Advanced: ["Sophomore Advanced Russian", "College Russian"],
+  Heritage: ["Freshman Heritage Russian", "Sophomore Heritage Russian", "College Heritage Russian"],
+  Business: ["Russian in Business"]
+} as const satisfies Record<classType, string[]>;
+
+function getGeneralClassType(specificClass: string): string | null {
+  const types = Object.keys(classTypes) as Array<keyof typeof classTypes>;
+
+  return (
+    types.find(type => classTypes[type].includes(specificClass)) || null
+  );
+}
+
+const generalClassType = getGeneralClassType(props.course.class_type)
+
 </script>
 
 <style scoped>
