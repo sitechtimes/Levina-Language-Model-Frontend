47706113ff0044566d820f1c444d36eec7098a6c
christina chen <158211764+christinac09@users.noreply.github.com>
2025-12-23 10:33:44 -0500

    Update roster.vue

--- pages/teacher/course/[classCode]/roster.vue
+++ pages/teacher/course/[classCode]/roster.vue
@@ -6,6 +6,34 @@
 
 <script setup lang="ts">
 const students = [{name: "John Doe"}]
+// /courses/1/students/ returns:
+// [
+//   {
+//     "id": 1,
+//     "email": "student1@example.com",
+//     "first_name": "Student",
+//     "last_name": "One",
+//     "user_type": 0
+//   },
+//   {
+//     "id": 2,
+//     "email": "student2@example.com",
+//     "first_name": "Student",
+//     "last_name": "Two",
+//     "user_type": 0
+//   },
+//   {
+//     "id": 3,
+//     "email": "sam.kipnis@gmail.com",
+//     "first_name": "Sam",
+//     "last_name": "Kipnis",
+//     "user_type": 0
+//   }
+// ]
+async function getStudents(params:type) {
+    
+}
+onMounted(getStudents)
 </script>
 
 <style scoped>
