03b55d4d1ff5ac4acc98fb5d2b9c8a1a5f77fa6b
Gabriel Liberov <gabruha19@gmail.com>
2026-01-28 10:55:44 -0500

    Update [courseID].vue

--- pages/teacher/course/[courseID].vue
+++ pages/teacher/course/[courseID].vue
@@ -10,9 +10,9 @@
 
 
        <div class="flex items-center justify-center gap-4">
-           <TeacherCourseActionButton type="link" :to="`/teacher/course/{classCodePlaceholder}/roster`" img="https://cdn2.iconfinder.com/data/icons/instagram-ui/48/jee-74-512.png" text="View Students"/>
-           <TeacherCourseActionButton type="link" :to="`/teacher/create-assignment`" img="https://png.pngtree.com/png-clipart/20230405/original/pngtree-assignment-line-icon-png-image_9025828.png" text="Create Assignment"/>
-           <TeacherCourseActionButton type="button" img="https://www.freeiconspng.com/uploads/trash-can-icon-18.png" text="Delete Course" class="hover:bg-red-400" @on-click="deleteCourse()" />
+           <TeacherCourseActionButton type="link" :to="`/teacher/course/{classCodePlaceholder}/roster`" img="/ui/users.svg" text="View Students"/>
+           <TeacherCourseActionButton type="link" :to="`/teacher/create-assignment`" img="/ui/document.svg" text="Create Assignment"/>
+           <TeacherCourseActionButton type="button" img="/ui/trash.svg" text="Delete Course" class="hover:bg-red-400" @on-click="deleteCourse()" />
           </div>
      </div>
 
