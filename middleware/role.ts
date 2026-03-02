39837410f896ae418023ad5fdf7fbd4b3340ab81
christina chen <158211764+christinac09@users.noreply.github.com>
2026-03-02 10:28:26 -0500

    Update role.ts

--- middleware/role.ts
+++ middleware/role.ts
@@ -6,8 +6,4 @@ export default defineNuxtRouteMiddleware((to) => {
   if (!userStore.isAuth) return navigateTo("/login", { redirectCode: 301 });
 
   if ((to.path.startsWith('/student') && userStore.userType !== 'student') || (to.path.startsWith('/teacher') && userStore.userType !== 'teacher')) return navigateTo(`/${userStore.userType}/dashboard`, { redirectCode: 301 });
-
-  //if (userStore.userType !== to.meta.role) return navigateTo(`/${userStore.userType}/dashboard`, { redirectCode: 301 });
 });
-
-// check that this works for admin
