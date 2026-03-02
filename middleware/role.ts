97d08fabdb56402302b2b5735edd58e409068f8a
christina chen <158211764+christinac09@users.noreply.github.com>
2026-03-02 10:33:22 -0500

    ?

--- middleware/role.ts
+++ middleware/role.ts
@@ -5,5 +5,8 @@ export default defineNuxtRouteMiddleware((to) => {
 
   if (!userStore.isAuth) return navigateTo("/login", { redirectCode: 301 });
 
+  const basePath = to.path.split('/')[1]; // 'student', 'teacher'
+  console.log(basePath)
+
   if ((to.path.startsWith('/student') && userStore.userType !== 'student') || (to.path.startsWith('/teacher') && userStore.userType !== 'teacher')) return navigateTo(`/${userStore.userType}/dashboard`, { redirectCode: 301 });
 });
